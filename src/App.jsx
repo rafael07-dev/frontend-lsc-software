62d107587790c047049564d784890f13a9d004b3
rafael07-dev <deralato817@gmail.com>
2025-05-15 09:15:20 -0500

    using routers

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,36 @@
-import { Header } from './components/Header'
-import './App.css'
+import { Header } from './components/Header';
+import { Login } from './components/Login';
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import AuthProvider from './context/AuthProvider';
+import Register from './components/Register';
+import { ToastContainer } from 'react-toastify';
+import ConfirmationEmail from './components/ConfirmationEmail';
+import Diccionary from './components/Diccionary';
+import Footer from './components/Footer';
 
 function App() {
-
   return (
-    <>
-      <Header/>
-      <main>
-
-      </main>
-    </>
-  )
+    <BrowserRouter>
+      <AuthProvider>
+        <div className="flex flex-col min-h-screen">
+          <Header />
+          <main className="flex-grow">
+            <Routes>
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/confirmation" element={<ConfirmationEmail />} />
+              <Route path="/" element={<Home />} />
+              <Route path="/diccionario" element={<Diccionary />} />
+            </Routes>
+            <ToastContainer />
+          </main>
+          <Footer />
+        </div>
+      </AuthProvider>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
