d39be1299245fa7eba855ed3eb0edef71227606d
rafael07-dev <deralato817@gmail.com>
2025-05-28 14:21:48 -0500

    add sub-route editMedia

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import AdminPanel from './components/AdminPanel';
 import Words from './components/Words';
 import Media from './components/Media';
 import AddMedia from './components/AddMedia';
+import EditMedia from './components/EditMedia';
 
 function App() {
   return (
@@ -33,6 +34,7 @@ function App() {
                 <Route path='words' element={<Words/>} />
                 <Route path='media' element={<Media />} >
                   <Route path='add' element={<AddMedia/>} />
+                  <Route path='edit/:id' element={<EditMedia />} />
                 </Route>
               </Route>
             </Routes>
