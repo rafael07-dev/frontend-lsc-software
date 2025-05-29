0fa5fd267d5055b409101d183654ec0ba2cff1a1
rafael07-dev <deralato817@gmail.com>
2025-05-29 08:14:15 -0500

    add modals to edit media and add media

--- src/components/Media.tsx
+++ src/components/Media.tsx
@@ -1,13 +1,49 @@
-import { Link, Outlet, useMatch } from "react-router-dom";
+import { Outlet, useMatch } from "react-router-dom";
 import Pagination from "./Pagination";
 import { useMedia } from "../hooks/useMedia";
 import VideoList from "./VideoList";
+import { useState } from "react";
+import EditMediaModal from "./EditMediaModal";
+import AddMediaModal from "./AddMediaModal";
+
+interface Video {
+    id: number;
+    giffUrl: string;
+    word: {
+        id: number;
+        word: string;
+        letter: {
+            id: number;
+            letter: string;
+        }
+    }
+}
 
 const Media = () => {
 
     const { videos, totalPages, goToPage, currentPageVideos } = useMedia()
     const isAdding = useMatch("/admin/media/add")
     const isEditing = useMatch("/admin/media/edit/:id")
+    const [showEditModal, setShowEditModal] = useState<boolean>(false)
+    const [showAddModal, setShowAddModal] = useState<boolean>(false)
+    const [currentVideo, setCurrentVideo] = useState<Video | null>(null)
+
+    function onCloseEditModal() {
+        setShowEditModal(false)
+    }
+
+    function handleEditModal(v: Video) {
+        setCurrentVideo(v)
+        setShowEditModal(true)
+    }
+
+    function onCloseAddModal() {
+        setShowAddModal(false)
+    }
+
+    function handleAddModal() {
+        setShowAddModal(true)
+    }
 
     if (isAdding || isEditing) {
         return <Outlet />;
@@ -17,11 +53,23 @@ const Media = () => {
         <div>
 
             <div className="flex justify-start mb-4">
-                <Link to="/admin/media/add" className="bg-blue-600 hover:bg-blue-700 text-white px-3 py-1.5 rounded text-sm">
-                    Agregar video
-                </Link>
+                <button 
+                    onClick={handleAddModal}
+                    className="bg-blue-600 hover:bg-blue-700 text-white px-3 py-1.5 rounded text-sm"
+                >
+                    Agregar seña
+                </button>
             </div>
-            <VideoList videos={videos}/>
+            <AddMediaModal
+                isVisible={showAddModal}
+                onClose={onCloseAddModal} 
+            />
+            <EditMediaModal 
+                isVisible={showEditModal} 
+                video={currentVideo} 
+                onClose={onCloseEditModal}
+            />
+            <VideoList videos={videos} currentVideo={handleEditModal}/>
             <Pagination pages={totalPages} currentPage={currentPageVideos} goToPage={goToPage}/>
         </div>
     );
