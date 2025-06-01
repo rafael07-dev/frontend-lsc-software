22adfc02befd4313926df99409f1b5bc980445ec
rafael07-dev <deralato817@gmail.com>
2025-06-01 12:08:17 -0500

    add hook to hands detection

---
+++ src/hooks/useHandDetection.ts
@@ -0,0 +1,52 @@
+import { useEffect, useRef } from "react";
+import { Hands, Results } from "@mediapipe/hands";
+import * as cam from "@mediapipe/camera_utils";
+
+export function useHandDetection(onResults: (results: Results) => void) {
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const cameraRef = useRef<cam.Camera | null>(null);
+  const hasInitialized = useRef(false);
+
+  useEffect(() => {
+    if (!hasInitialized.current && videoRef.current) {
+      const hands = new Hands({
+        locateFile: (file) =>
+          `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`,
+      });
+
+      // En useHandDetection.js, modifica los parámetros
+      hands.setOptions({
+        maxNumHands: 1,
+        modelComplexity: 1,
+        minDetectionConfidence: 0.7,  // Reducido de 0.7
+        minTrackingConfidence: 0.7,   // Reducido de 0.7
+      });
+
+      hands.onResults(onResults);
+
+      cameraRef.current = new cam.Camera(videoRef.current, {
+        onFrame: async () => {
+          if (videoRef.current?.readyState === 4) {
+            try {
+              await hands.send({ image: videoRef.current });
+            } catch (error) {
+              console.error("Error sending frame to model:", error);
+            }
+          }
+        },
+        width: 640,
+        height: 480,
+      });
+
+      cameraRef.current.start();
+      hasInitialized.current = true;
+
+      return () => {
+        cameraRef.current?.stop();
+        hands.close();
+      };
+    }
+  }, [onResults]);
+
+  return { videoRef };
+}
