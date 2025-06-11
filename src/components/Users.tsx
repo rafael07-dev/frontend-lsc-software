1815f113687d190d7efe7fc9b43232a3bbe8e67a
rafael07-dev <deralato817@gmail.com>
2025-06-11 08:52:12 -0500

    ussing userModal and useUser

--- src/components/Users.tsx
+++ src/components/Users.tsx
@@ -1,7 +1,10 @@
+import { useUserModal } from "../hooks/useUserModal";
 import { useUsers } from "../hooks/useUsers";
+import UserModal from "./UserModal";
 
 const Users = () => {
     const { users } = useUsers();
+    const { isVisible, openUserModal, closeUserModal, currentUser } = useUserModal();
 
     return (
         <div className="max-w-4xl mx-auto px-4 py-8">
@@ -9,34 +12,38 @@ const Users = () => {
                 Usuarios disponibles en el sistema
             </h1>
 
+
             <div className="grid gap-4">
                 {users?.map(user => (
-                    <div
-                        key={user.id}
-                        className="p-4 border rounded-2xl shadow-md bg-white hover:shadow-lg transition-shadow duration-200"
-                    >
-                        <p className="text-lg font-semibold text-gray-900">
-                            {user.firstName} {user.lastName}
-                        </p>
-                        <p className="text-sm text-gray-600">
-                            Usuario: <span className="font-medium">{user.username}</span>
-                        </p>
+                    <>
+                        <div
+                            onClick={() => openUserModal(user)}
+                            key={user.id}
+                            className="cursor-pointer p-4 border rounded-2xl shadow-md bg-white hover:shadow-lg transition-shadow duration-200"
+                        >
+                            <p className="text-lg font-semibold text-gray-900">
+                                {user.firstName} {user.lastName}
+                            </p>
+                            <p className="text-sm text-gray-600">
+                                Usuario: <span className="font-medium">{user.username}</span>
+                            </p>
 
-                        <p>
-                            roles: {user.roles }
-                        </p>
-                        <p className="text-sm text-gray-600">
-                            Estado:{" "}
-                            <span
-                                className={`font-bold ${
-                                    user.enabled ? "text-green-600" : "text-red-600"
-                                }`}
-                            >
-                                {user.enabled ? "Activo" : "Inactivo"}
-                            </span>
-                        </p>
-                    </div>
+                            <p>
+                                roles: {user.roles}
+                            </p>
+                            <p className="text-sm text-gray-600">
+                                Estado:{" "}
+                                <span
+                                    className={`font-bold ${user.enabled ? "text-green-600" : "text-red-600"
+                                        }`}
+                                >
+                                    {user.enabled ? "Activo" : "Inactivo"}
+                                </span>
+                            </p>
+                        </div>
+                    </>
                 ))}
+                <UserModal isVisible={isVisible} user={currentUser} closeUserModal={closeUserModal} />
             </div>
         </div>
     );
